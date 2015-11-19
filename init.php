6605bef8b17f8110dc752b61be429e6178432587
cdowdy <cdowdy46@gmail.com>
2015-11-19 08:54:13 -0500

    updated namespace for the extension

--- init.php
+++ init.php
@@ -1,5 +1,11 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Cory
+ * Date: 10/29/2015
+ * Time: 10:23 AM
+ */
 
-namespace Bolt\Extension\cdowdy\ResponsiveImages;
+namespace Bolt\Extension\cdowdy\boltresponsiveimages;
 
 $app['extensions']->register(new Extension($app));
