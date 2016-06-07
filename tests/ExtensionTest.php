4c78422cf0f937d7c824d603aec468933faddf40
cdowdy <cdowdy46@gmail.com>
2016-06-07 12:08:17 -0400

    tests

---
+++ tests/ExtensionTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Bolt\Extension\cdowdy\boltresponsiveimages\Tests;
+
+use Bolt\Tests\BoltUnitTest;
+use Bolt\Extension\cdowdy\boltresponsiveimages\Extension;
+
+/**
+ * Ensure that the ExtensionName extension loads correctly.
+ *
+ */
+class ExtensionTest extends BoltUnitTest
+{
+    public function testExtensionRegister()
+    {
+        $app = $this->getApp();
+        $extension = new Extension($app);
+        $app['extensions']->register( $extension );
+        $name = $extension->getName();
+        $this->assertSame($name, 'ExtensionName');
+        $this->assertSame($extension, $app["extensions.$name"]);
+    }
+}
